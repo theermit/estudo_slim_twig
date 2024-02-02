676c454a6e0e0ac076a0195f4d0a3c7e37e15a6c
benhur alencar azevedo <benhur.azevedo@hotmail.com>
2024-02-02 15:50:21 -0300

    primeiro commit

---
+++ lib/config/Config.php
@@ -0,0 +1,28 @@
+<?php
+namespace lib\config;
+class Config 
+{
+    public static function getConfig() : array
+    {
+        // gera a configuração com base no arquivo .env
+        $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
+        $dotenv->load();
+
+        $config = [ 'db' => [], 'ambiente_app' => '', 'JWT' => [], 'cache_to_view' => ''];
+
+        $config['db']['SGDB'] = $_ENV['SGDB'];
+        switch($_ENV['SGDB'])
+        {
+            case 'SQLITE':
+                $config['db']['DSN'] = "sqlite:" . $_ENV['DSN'];
+                break;
+            case 'POSTGRESQL':
+                break;
+        }
+        $config['ambiente_app'] = $_ENV['AMBIENTE'];
+        $config['base_path'] = $_ENV['BASE_PATH'];
+        $config['cache_to_view'] = $_ENV['CACHE_TO_VIEW'] == 'true';
+
+        return $config;
+    }
+}
